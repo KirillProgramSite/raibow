1a344ddd2cb518799ac5fecf600f3dd3de8da758
Kirill Proskurin <78263182+KirillProgramSite@users.noreply.github.com>
2024-06-16 01:11:13 +0300

    Add files via upload

---
+++ rainbow-main/rainbow-main/app.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var faqCards = document.querySelectorAll('.faq-card');
+
+    faqCards.forEach(function(faqCard) {
+        var title = faqCard.querySelector('.faq-card-title');
+        var text = faqCard.querySelector('.faq-card-text');
+        var arrow = title.querySelector('.wrapper.faq-card-title img');
+
+        title.addEventListener('click', function() {
+            faqCard.classList.toggle('active')
+            toggleArrow(arrow);
+        });
+    });
+
+    function toggleAccordion(element) {
+        if (element.style.maxHeight) {
+            element.style.maxHeight = null;
+        } else {
+            element.style.height = 150 + "vw";
+        }
+    }
+
+    function toggleArrow(arrow) {
+        arrow.classList.toggle('rotate');
+    }
+});
